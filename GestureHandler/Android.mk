23633f6757e831cdadcb92ce57b2a1991b102939
Mihran Thalhath <mihranz7@gmail.com>
2018-07-06 20:04:28 +0530

    Revert "msm8974-common: GestureHandler: Add support for additional gestures"
    This reverts commit ac53dd9be48dc1d099702bb41fefc5cb648f5956.

--- GestureHandler/Android.mk
+++ GestureHandler/Android.mk
@@ -11,7 +11,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := GestureHandler
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
